5949d740c1cf1e05eaa76629a5bb0f256f123056
Tanaya Pawar <tanayapawar02@gmail.com>
2024-11-07 14:25:44 +0530

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Homepage from './pages/homepage'
+const App = () => {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <>
+    <section id='homepage'>
+      <h1>ChainElect</h1>
+    </section>
+    </>
+  )
+}
+
+export default Homepage
