1f8ce7ca684c6695fc47e5c970e28ca5a17906e4
Tanaya Pawar <tanayapawar02@gmail.com>
2025-01-17 15:29:03 +0530

    hardhat Set up

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Homepage from './pages/Homepage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
+import VotersPage from './pages/VotersPage'
+
+
 const App = () => {
   return (
     <div>
       <Routes>
               <Route path='/' element={<Homepage />} />
               <Route path='/register' element={<Register />} />
+              <Route path='/VotersPage'element={<VotersPage />} />
+
       </Routes>
     </div>
   )
