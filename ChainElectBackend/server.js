7ef45caeba37afc8dd82abb31a178c5153956548
AtharvaPC <baodhankaratharva@gmail.com>
2025-03-23 09:22:18 +0530

    deploy

--- ChainElectBackend/server.js
+++ ChainElectBackend/server.js
@@ -237,5 +237,5 @@ app.get('/voters/:voter_id', async (req, res) => {
 });
 
 // Start server
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
